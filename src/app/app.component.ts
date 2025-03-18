37200593bfff0fa965c0d18951cfe2f4eca7b4f3
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2025-03-18 18:02:14 +0100

    housekeeping: Removing boilerplate from default starter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'udemy-angular-easytask';
+  title = 'Easy Task';
 }
