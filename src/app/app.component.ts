8d7806602acb8ea3ef8bf5ce493b11436e776d32
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2025-03-19 14:56:11 +0100

    feat: Add user component to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
