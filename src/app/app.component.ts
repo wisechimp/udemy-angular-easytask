e3a289130ded8feb3117b236d16ca1fd1a9fee45
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2025-05-21 09:28:28 +0200

    feat: Making the user component reusable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
+import { DUMMY_USERS } from './data/users-data';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,5 @@ import { UserComponent } from './components/user/user.component';
 })
 export class AppComponent {
   title = 'EasyTask';
+  users = DUMMY_USERS;
 }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,21 +1,18 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../../data/users-data';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
